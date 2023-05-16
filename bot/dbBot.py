ae3b7a5d4a5acc7d470e84e6f72a9970f1a8f529
GH0ST1NN3T <renatocoutinho10@gmail.com>
2023-05-16 14:04:55 -0300

    a
    mudança de enreceço

--- bot/dbBot.py
+++ bot/dbBot.py
@@ -1,10 +1,11 @@
 import discord
+import time
 import datetime
 import math
 from discord import app_commands
 from discord.ext import commands
 import random
-#z0n4 571722603920752650
+#z0n4 571722603920752650 guild = discord.Object(id=571722603920752650
 #ads 1042560674812923914
 class abot(discord.Client):
     def __init__(self):
@@ -14,34 +15,36 @@ class abot(discord.Client):
     async def on_ready(self):
         await self.wait_until_ready()
         if not self.synced:
-            await tree.sync(guild = discord.Object(id=571722603920752650))
+            await zonbot.sync()
             self.synced = True
         print(f"Entramos como {self.user}.")
         
 client = abot()
-tree = app_commands.CommandTree(client)
-@tree.command(name= "teste", description = "testando", guild = discord.Object(id = 571722603920752650))
+zonbot = app_commands.CommandTree(client)
+@zonbot.command(name= "teste", description = "testando",)
 async def self(interaction: discord.Interaction):
     await interaction.response.send_message(f"Olá! eu fui feito a base do Discord.py . Sim eu tô funcionando :thumbsup:")   
 
-@tree.command(name= "pergunte", description = "e o bot responde", guild = discord.Object(id = 571722603920752650))
+@zonbot.command(name= "pergunte", description = "e o bot responde")
 async def self(interaction: discord.Interaction, pergunta: str):
-    respostas= ["não sei", "quem sabe","se eu soubesse eu te falava","não :)","a va ti lascar rapa","Sim","para com issae bicho"]
+    respostas= ["não sei", "quem sabe","se eu soubesse eu te falava","não :)","a va ti lascar rapa","Sim","para com issae bicho","IMPOSSÍVEL","só tem um jeito: capotando na porrada","olha o dicionário q tu descobre"]
     await interaction.response.send_message(f"**Pergunta: **{pergunta}\n**Resposta**: {random.choice(respostas)}")   
 
-@tree.command(name="randomfoto",description="acha uma foto ae",guild = discord.Object(id = 571722603920752650))
+@zonbot.command(name="randomfoto",description="acha uma foto ae")
 async def get_random_image(img):
     
-    num= random.randint(0, 1920)    
+    num= random.randint(144, 1920)   
+    num2= random.randint(144, 1920) 
+
     Embed = discord.Embed(
         title="Foto buscada com sucesso",
-        description="Lembrando q é random msm",
+        description=f"Lembrando q é random msm",
         color=discord.Color.random(),
         
         )
-    Embed.set_image(url="https://api.lorem.space/image?w=1920&h=1080")
+    Embed.set_image(url=(f"https://picsum.photos/{num}/{num2}"))
     await img.channel.send(embed=Embed)
-@tree.command(name= "surra", description = "em alguém", guild = discord.Object(id = 571722603920752650))
+@zonbot.command(name= "surra", description = "em alguém")
 async def self(interaction: discord.Interaction, atacante: str, atacado: str, objeto:str, tempo:float):
     if type(tempo) != float:
         await interaction.response.send_message(f"comando errado, o tempo deve ser apenas numérico")
@@ -49,19 +52,20 @@ async def self(interaction: discord.Interaction, atacante: str, atacado: str, ob
         await interaction.response.send_message(f"Por pedido de {atacante}, o alvo {atacado} vai levar uma surra de {objeto} por {round(tempo)} minutos")
     
 
-@tree.command(name="calculadora", description="calcula algo",guild = discord.Object(id = 571722603920752650))
+@zonbot.command(name="calculadora", description="calcula algo")
 @app_commands.describe(operacoes='tipos de operações')
 @app_commands.choices(operacoes=[
     discord.app_commands.Choice(name='vezes',value=1),
     discord.app_commands.Choice(name='divisao',value=2),
     discord.app_commands.Choice(name='subtraçao',value=3),
     discord.app_commands.Choice(name='adiçao',value=4),
-    discord.app_commands.Choice(name='porcentagem',value=5)
+    #discord.app_commands.Choice(name='porcentagem',value=5)
 ])
 async def self(interaction: discord.Interaction,primeiro_numero:float,operacoes:discord.app_commands.Choice[int], segundo_numero:float):
     
     if operacoes.name == "vezes":
         resultado = primeiro_numero*segundo_numero
+        #@app_commands.describe(operacoes='= {}'.format(str(num)))
         await interaction.response.send_message(f" a resposta é {resultado}")
     elif operacoes.name == "divisao":
         resultado = primeiro_numero/segundo_numero
@@ -72,7 +76,8 @@ async def self(interaction: discord.Interaction,primeiro_numero:float,operacoes:
     elif operacoes.name == "adiçao":
         resultado = primeiro_numero+segundo_numero
         await interaction.response.send_message(f" a resposta é {resultado}") 
+@zonbot.command(name= "fotocj", description = "Foto do Cejota",)
+async def self(interaction: discord.Interaction):
+    await interaction.response.send_message(f"https://c1.wallpaperflare.com/preview/240/187/733/carbon-black-barbecue-charcoal.jpg") 
 
-
-
-client.run("MTA0MzAxMTY2Mzc0MzQyNjYyMg.GYRr25.uK3qRg8lREYSs1B1QPAIAo86_f1gWKuNnHeDMM")
+client.run("MTA0MzAxMTY2Mzc0MzQyNjYyMg.GUADCe.Axu9ziWGTWKZidyxGJ9U2VFOB1p254LbHcO__Y")
